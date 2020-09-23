5c63803a1a7975eaa8fea532c71c9bd273b97efc
Eduardo <eduardo.dias@zemopay.com>
2020-09-23 02:12:48 -0300

    add docker config and update .env-example

--- routes/web.php
+++ routes/web.php
@@ -1,4 +1,7 @@
 <?php
+$router->get('/', function () use ($router) {
+    return $router->app->version();
+});
 
 $router->group(['prefix' => 'transaction'], function($router) {
     $router->post('/', 'TransactionController@peerToPeer');

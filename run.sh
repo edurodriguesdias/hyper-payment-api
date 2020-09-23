f6817c00f3f3c79c6d77fff0551d4c7ba4351a94
Eduardo <eduardo.dias@zemopay.com>
2020-09-23 19:55:38 -0300

    edit run.sh file

--- run.sh
+++ run.sh
@@ -16,6 +16,8 @@ docker exec -it php php /var/www/html/artisan migrate
 
 echo Running seeds
 docker exec -it php php /var/www/html/artisan db:seed
+echo --------API SET UP FINISH--------
+echo send a POST request to http://localhost/transaction and enjoy it!
 
 echo Queue working
 docker exec -it php php /var/www/html/artisan queue:work --queue=high,medium,low,default
